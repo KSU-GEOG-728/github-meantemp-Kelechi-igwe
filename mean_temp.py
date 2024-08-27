9a60d947073718b4bdb8f0048615c6d3760fe2c8
Kelechi Igwe <igwekelechi.e@gmail.com>
2024-08-27 17:41:56 -0500

    Update mean_temp.py
    added yesterday and today's high temperatures. Then specified an equation to calculate the average of both values.

--- mean_temp.py
+++ mean_temp.py
@@ -3,20 +3,24 @@
 
 """
     File name: mean_temp.py
-    Author: ENTER YOUR  NAME
+    Author: Kelechi Igwe
     Description:  Calculates mean daily temperature from two user defined variables representing a daily high (x) and low (y) temperature
-    Date created: ENTER TODAY'S DATE in MM/DD/YYYY FORMAT (e.g., 08/23/2023)
+    Date created: ENTER TODAY'S DATE in 08/27/2024 FORMAT (e.g., 08/23/2023)
     Python Version: 3.9.16
 """
 
 # Assign variable x the value for the previous day's high temperature
-x = 
+x = 93
 
 # Assign variable y the value for the previous day's low temperature
-y =  
+y = 94
 
 # Create variable z and use mathematical operators to calculate the mean temperature
-z = 
+z = (x+y)/2
 
 # Use this custom statement that prints the result to screen
 print("Yesterday's mean daily temperature was {0} degrees.".format(z))
+
+
+# Output
+# Yesterday's mean daily temperature was 93.5 degrees.
